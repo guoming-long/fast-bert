eb73b527c3e3a649010aa924437bd3b11a9d3be5
guoming-long <61188783+guoming-long@users.noreply.github.com>
2020-03-05 05:04:26 +0800

    Update metrics.py

--- fast_bert/metrics.py
+++ fast_bert/metrics.py
@@ -27,15 +27,24 @@ def recall_macro(y_pred: Tensor, y_true: Tensor):
 def recall_by_class(y_pred: Tensor, y_true: Tensor, labels: list = labels_list):
     y_pred = y_pred.cpu()
     y_pred = np.argmax(y_pred, axis=1)
-    y_pred = encoder.fit_transform(y_pred)
     print(y_pred)
     y_true = y_true.cpu()
     y_true = np.argmax(y_true, axis=1)
-    y_true = encoder.fit_transform(y_true)
     d = {}
     for i in range(len(labels)): 
-        out_pred = y_pred[:,i]
-        out_true = y_true[:,i]
+        out_pred = []
+        out_true = []
+        for j in y_pred:
+            if j == i:
+                out_pred = out_pred.append(1)
+            else:
+                out_pred = out_pred.append(0)
+        print(out_pred)
+        for j in y_true:
+            if j == i:
+                out_true = out_true.append(1)
+            else:
+                out_true = out_true.append(0)
         d[labels[i]] = recall_score(out_pred, out_true, average='micro')
     return d
 
